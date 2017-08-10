5c4fc8981fc5c5bb3f5e3daeb2f1445b84db297a
Rob Archibald <rob.archibald@endfirst.com>
2017-08-10 10:24:22 -0700

    test improvements

--- doc.go
+++ doc.go
@@ -13,10 +13,10 @@ import (
 
 var (
 	errNoFields    = errors.New("No fields")
-	errFibShort    = errors.New("file information block too short")
-	errClxShort    = errors.New("clx block too short")
+	errFibInvalid  = errors.New("file information block validation failed")
 	errTable       = errors.New("cannot find table stream")
 	errInvalidClx  = errors.New("expected last aCP value to equal fib.cpLength (2.8.35)")
+	errDocEmpty    = errors.New("WordDocument not found")
 	errDocShort    = errors.New("wordDoc block too short")
 	errInvalidPcdt = errors.New("expected clxt to be equal 0x02")
 )
@@ -188,12 +188,9 @@ func getClx(table *mscfb.File, fib *fib) (*clx, error) {
 
 func readClx(table *mscfb.File, fib *fib) ([]byte, error) {
 	b := make([]byte, fib.fibRgFcLcb.lcbClx)
-	i, err := table.ReadAt(b, int64(fib.fibRgFcLcb.fcClx))
+	_, err := table.ReadAt(b, int64(fib.fibRgFcLcb.fcClx))
 	if err != nil {
 		return nil, err
-	}
-	if i != fib.fibRgFcLcb.lcbClx {
-		return nil, errClxShort // clx is not long enough
 	}
 	return b, nil
 }
@@ -254,33 +251,31 @@ func parseFcCompressed(fcData []byte) *fcCompressed {
 // parse File Information Block (section 2.5.1)
 func getFib(wordDoc *mscfb.File) (*fib, error) {
 	if wordDoc == nil {
-		return nil, errors.New("WordDocument not found")
+		return nil, errDocEmpty
 	}
 
 	b := make([]byte, 894) // get FIB block up to FibRgFcLcb97
-	i, err := wordDoc.Read(b)
+	i, err := wordDoc.ReadAt(b, 0)
 	if err != nil {
 		return nil, err
 	}
-	if i < 894 {
-		return nil, errFibShort // fib is not long enough
-	}
+	fmt.Println(i)
 
 	fibBase := getFibBase(b[0:32])
 
-	csw := int(binary.LittleEndian.Uint16(b[32:34])) * 2 // in bytes
-	fibRgwStart := 34
-	//fibRgW := getFibRgW(b[fibRgwStart : fibRgwStart+csw])
+	fibRgW, csw, err := getFibRgW(b, 32)
+	if err != nil {
+		return nil, err
+	}
 
-	cslw := int(binary.LittleEndian.Uint16(b[fibRgwStart+csw:fibRgwStart+csw+2])) * 4 // in bytes
-	fibRgLwStart := fibRgwStart + csw + 2
-	fibRgLw := getFibRgLw(b[fibRgLwStart : fibRgLwStart+cslw*4])
+	fibRgLw, cslw, err := getFibRgLw(b, 34+csw)
+	if err != nil {
+		return nil, err
+	}
 
-	cbRgFcLcb := int(binary.LittleEndian.Uint16(b[fibRgLwStart+cslw : fibRgLwStart+cslw+2]))
-	fibRgFcLcbStart := fibRgLwStart + cslw + 2
-	fibRgFcLcb := getFibRgFcLcb(b[fibRgFcLcbStart : fibRgFcLcbStart+cbRgFcLcb])
+	fibRgFcLcb, cbRgFcLcb, err := getFibRgFcLcb(b, 34+csw+2+cslw)
 
-	return &fib{base: *fibBase, csw: csw, cslw: cslw, fibRgLw: *fibRgLw, fibRgFcLcb: *fibRgFcLcb, cbRgFcLcb: cbRgFcLcb}, nil
+	return &fib{base: *fibBase, csw: csw, cslw: cslw, fibRgW: *fibRgW, fibRgLw: *fibRgLw, fibRgFcLcb: *fibRgFcLcb, cbRgFcLcb: cbRgFcLcb}, err
 }
 
 // parse FibBase (section 2.5.2)
@@ -290,16 +285,31 @@ func getFibBase(fib []byte) *fibBase {
 	return &fibBase{fWhichTblStm: fWhichTblStm}
 }
 
+func getFibRgW(fib []byte, start int) (*fibRgW, int, error) {
+	if start+2 >= len(fib) { // must be big enough for csw
+		return &fibRgW{}, 0, errFibInvalid
+	}
+
+	csw := int(binary.LittleEndian.Uint16(fib[start:start+2])) * 2 // in bytes
+	return &fibRgW{}, csw, nil
+}
+
 // parse FibRgLw (section 2.5.4)
-func getFibRgLw(fib []byte) *fibRgLw {
-	const ccpTextLoc = 3 * 4
-	const ccpFtnLoc = 4 * 4
-	const ccpHddLoc = 5 * 4
-	const ccpMcrLoc = 6 * 4
-	const ccpAtnLoc = 7 * 4
-	const ccpEdnLoc = 8 * 4
-	const ccpTxbxLoc = 9 * 4
-	const ccpHdrTxbxLoc = 10 * 4
+func getFibRgLw(fib []byte, start int) (*fibRgLw, int, error) {
+	fibRgLwStart := start + 2 // skip cslw
+	ccpTextLoc := fibRgLwStart + 3*4
+	ccpFtnLoc := fibRgLwStart + 4*4
+	ccpHddLoc := fibRgLwStart + 5*4
+	ccpMcrLoc := fibRgLwStart + 6*4
+	ccpAtnLoc := fibRgLwStart + 7*4
+	ccpEdnLoc := fibRgLwStart + 8*4
+	ccpTxbxLoc := fibRgLwStart + 9*4
+	ccpHdrTxbxLoc := fibRgLwStart + 10*4
+	if ccpHdrTxbxLoc+4 >= len(fib) { // must be big enough for ccpHdrTxbx
+		return &fibRgLw{}, 0, errFibInvalid
+	}
+
+	cslw := int(binary.LittleEndian.Uint16(fib[start:start+2])) * 4 // in bytes
 	ccpText := int(binary.LittleEndian.Uint32(fib[ccpTextLoc : ccpTextLoc+4]))
 	ccpFtn := int(binary.LittleEndian.Uint32(fib[ccpFtnLoc : ccpFtnLoc+4]))
 	ccpHdd := int(binary.LittleEndian.Uint32(fib[ccpHddLoc : ccpHddLoc+4]))
@@ -309,6 +319,7 @@ func getFibRgLw(fib []byte) *fibRgLw {
 	ccpTxbx := int(binary.LittleEndian.Uint32(fib[ccpTxbxLoc : ccpTxbxLoc+4]))
 	ccpHdrTxbx := int(binary.LittleEndian.Uint32(fib[ccpHdrTxbxLoc : ccpHdrTxbxLoc+4]))
 
+	// calculate cpLength. Used in PlcPcd verification (see section 2.8.35)
 	var cpLength int
 	if ccpFtn != 0 || ccpHdd != 0 || ccpMcr != 0 || ccpAtn != 0 || ccpEdn != 0 || ccpTxbx != 0 || ccpHdrTxbx != 0 {
 		cpLength = ccpFtn + ccpHdd + ccpMcr + ccpAtn + ccpEdn + ccpEdn + ccpTxbx + ccpHdrTxbx + ccpText + 1
@@ -316,16 +327,22 @@ func getFibRgLw(fib []byte) *fibRgLw {
 		cpLength = ccpText
 	}
 	return &fibRgLw{ccpText: ccpText, ccpFtn: ccpFtn, ccpHdd: ccpHdd, ccpMcr: ccpMcr, ccpAtn: ccpAtn,
-		ccpEdn: ccpEdn, ccpTxbx: ccpTxbx, ccpHdrTxbx: ccpHdrTxbx, cpLength: cpLength}
+		ccpEdn: ccpEdn, ccpTxbx: ccpTxbx, ccpHdrTxbx: ccpHdrTxbx, cpLength: cpLength}, cslw, nil
 }
 
 // parse FibRgFcLcb (section 2.5.5)
-func getFibRgFcLcb(fib []byte) *fibRgFcLcb {
-	const fcClxLoc = 66 * 4
-	const lsbClxLoc = 67 * 4
+func getFibRgFcLcb(fib []byte, start int) (*fibRgFcLcb, int, error) {
+	fibRgFcLcbStart := start + 2 // skip cbRgFcLcb
+	fcClxLoc := fibRgFcLcbStart + 66*4
+	lsbClxLoc := fibRgFcLcbStart + 67*4
+	if lsbClxLoc+4 >= len(fib) { // must be big enough for lsbClxLoc
+		return &fibRgFcLcb{}, 0, errFibInvalid
+	}
+
+	cbRgFcLcb := int(binary.LittleEndian.Uint16(fib[start : start+2]))
 	fcClx := int(binary.LittleEndian.Uint32(fib[fcClxLoc : fcClxLoc+4]))
 	lcbClx := int(binary.LittleEndian.Uint32(fib[lsbClxLoc : lsbClxLoc+4]))
-	return &fibRgFcLcb{fcClx: fcClx, lcbClx: lcbClx}
+	return &fibRgFcLcb{fcClx: fcClx, lcbClx: lcbClx}, cbRgFcLcb, nil
 }
 
 func getWordDocAndTables(r *mscfb.Reader) (*mscfb.File, *mscfb.File, *mscfb.File) {
--- doc_test.go
+++ doc_test.go
@@ -3,7 +3,19 @@ package doc2txt
 import (
 	"os"
 	"testing"
+
+	"github.com/richardlehane/mscfb"
 )
+
+var simpleDoc *mscfb.File
+var table *mscfb.File
+var reader *mscfb.Reader
+
+func init() {
+	f, _ := os.Open(`testData\simpleDoc.doc`)
+	reader, _ = mscfb.New(f)
+	simpleDoc, table, _ = getWordDocAndTables(reader)
+}
 
 func TestParseDoc(t *testing.T) {
 	f, err := os.Open(`testData\simpleDoc.doc`)
@@ -12,4 +24,35 @@ func TestParseDoc(t *testing.T) {
 		t.Error(err)
 	}
 
+}
+
+func TestGetFib(t *testing.T) {
+	_, err := getFib(nil)
+	if err != errDocEmpty {
+		t.Error("Expected error due to empty WordDoc")
+	}
+	if _, err := getFib(reader.File[2]); err == nil { // short mscfb.File
+		t.Error("expected error due to short file", err)
+	}
+	if _, err = getFib(reader.File[4]); err != errFibInvalid { // use wrong mscfb.File
+		t.Error("expected error due to corrupt file", err)
+	}
+
+	fib, _ := getFib(simpleDoc)
+	if fib.csw != 28 || fib.cslw != 88 || fib.cbRgFcLcb != 0x00B7 {
+		t.Error("expected valid sizes", fib.csw, fib.cslw, fib.cbRgFcLcb)
+	}
+	if fib.base.fWhichTblStm != 1 {
+		t.Error("expected table 1")
+	}
+	// No headers in simpleDoc, just "12345" in the text which apparently makes a ccpText of 6
+	// cpLength is calculated and should equal ccpText in this scenario
+	if fib.fibRgLw.ccpAtn != 0 || fib.fibRgLw.ccpEdn != 0 || fib.fibRgLw.ccpFtn != 0 || fib.fibRgLw.ccpHdd != 0 || fib.fibRgLw.ccpHdrTxbx != 0 ||
+		fib.fibRgLw.ccpMcr != 0 || fib.fibRgLw.ccpText != 6 || fib.fibRgLw.cpLength != 6 {
+		t.Error("expected valid fibRgLw", fib.fibRgLw)
+	}
+	// These are the values in the byte stream at the correct locations
+	if fib.fibRgFcLcb.fcClx != 5279 || fib.fibRgFcLcb.lcbClx != 21 {
+		t.Error("expected valid fibRgFcLcb", fib.fibRgFcLcb)
+	}
 }
